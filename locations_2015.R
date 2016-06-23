076ca9da37c8b2d0e1bb20d65ccb326387f4beb8
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2016-06-23 12:12:28 -0400

    a bit more work on inlcuding RI data: in locations_2015

--- locations_2015.R
+++ locations_2015.R
@@ -2,14 +2,17 @@ library(dplyr)
 dat15 <- read.csv("data/data_2015.csv")
 ri15 <- read.csv("data/ri_field_2015.csv")
 loc_all <- dat15 %>% 
-  select(waterbodyID, waterbodyName, longitudeSta, latitudeSta,orgID,state)
+  select(waterbodyID, waterbodyName, longitudeSta, latitudeSta,orgID,state,
+         sampleID,sampleDate,sampleRep)
 
 loc_ri <- ri15 %>%
-  select(waterbodyID, waterbodyName, longitudeSta, latitudeSta,orgID,state)
+  select(waterbodyID, waterbodyName, longitudeSta, latitudeSta,orgID,state,
+         sampleID,sampleDate,sampleRep)
   
-loc_all <- na.omit(rbind(loc_all,loc_ri))
+loc_all <- rbind(loc_all,loc_ri)
+loc_all <- loc_all[complete.cases(cbind(loc_all$longitudeSta,loc_all$latitudeSta)),]
 
-loc_all_u <- unique(rbind(loc_all,loc_ri))
+loc_all_u <- unique(loc_all)
 
 idx <- which(loc_all_u$longitudeSta > 0)
 should_be_lat <- loc_all_u$longitudeSta[idx] 
@@ -24,3 +27,7 @@ loc_all_u$longitudeSta[idx] <- loc_all_u$longitudeSta[idx] * -1
 plot(loc_all_u$longitudeSta,loc_all_u$latitudeSta)
 
 write.csv(loc_all_u,"locations_2015.csv",row.names = F)
+
+length(unique(loc_all_u$orgID))
+dim(unique(loc_all_u[,c("longitudeSta","latitudeSta")]))
+dim(unique(loc_all_u[,c("sampleID","sampleDate","sampleRep")]))
