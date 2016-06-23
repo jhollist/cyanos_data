7c3c21a249e60f3aa3588c6db61c1dce9a77e9b3
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2016-06-23 12:18:21 -0400

    another change

--- locations_2015.R
+++ locations_2015.R
@@ -1,6 +1,14 @@
 library(dplyr)
 dat15 <- read.csv("data/data_2015.csv")
-ri15 <- read.csv("data/ri_field_2015.csv")
+ri15_fld <- read.csv("data/ri_field_2015.csv")[,1:28]
+ri15_fld$sampleID <- factor(ri15_fld$sampleID)
+ri15_lab <- read.csv("data/ri_lab_2015.csv")[,1:17]
+ri15_lab$sampleID <- factor(ri15_lab$sampleID)
+ri15_d <- full_join(ri15_fld,ri15_lab) %>%
+  select_(.dots = names(dat15))
+dat15_all <- rbind(dat15,ri15_d)
+dat15_all <- dat15_all[complete.cases(dat15_all[c("ChlaUGL","PhycoUGL")]),]
+
 loc_all <- dat15 %>% 
   select(waterbodyID, waterbodyName, longitudeSta, latitudeSta,orgID,state,
          sampleID,sampleDate,sampleRep)
@@ -28,6 +36,7 @@ plot(loc_all_u$longitudeSta,loc_all_u$latitudeSta)
 
 write.csv(loc_all_u,"locations_2015.csv",row.names = F)
 
-length(unique(loc_all_u$orgID))
-dim(unique(loc_all_u[,c("longitudeSta","latitudeSta")]))
-dim(unique(loc_all_u[,c("sampleID","sampleDate","sampleRep")]))
+num_orgs <- length(unique(loc_all_u$orgID))
+num_wbd <- length(unique(loc_all_u$waterbodyName))
+num_locs <- dim(unique(loc_all_u[,c("longitudeSta","latitudeSta")]))[1]
+num_samples <- dim(unique(loc_all_u[,c("sampleID","sampleDate","sampleRep")]))[1]
